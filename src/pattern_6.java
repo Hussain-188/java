6af9af798ff6c042c694fb516158ad556cc1c2d7
Mohamed Hussain <23cs1242@stjosephstechnology.ac.in>
2025-08-08 22:52:29 +0530

    6th pattern

---
+++ src/pattern_6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pattern_6 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter num:");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int start=1;
+            if(i%2==0) start=0;
+            for(int j=1;j<=i;j++){
+                System.out.print(start);
+                start=1-start;
+            }
+            System.out.println();
+        }
+    }
+}
