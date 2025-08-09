e131b993d19608409512d34f2c636f9f7d7b1bcb
Mohamed Hussain <23cs1242@stjosephstechnology.ac.in>
2025-08-09 21:35:59 +0530

    10th pattern

---
+++ src/pattern_10.java
@@ -0,0 +1,12 @@
+public class pattern_10 {
+    public static void main(String args[]){
+        int n=5;
+        char start='A';
+        for(int i=1;i<=n;i++){
+            for(char ch=start;ch<=start+(n-i);ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
