efc8904b29643f118cb63bf6b844eb499a184cfc
Mohamed Hussain <23cs1242@stjosephstechnology.ac.in>
2025-08-10 13:04:27 +0530

    13th pattern

---
+++ src/pattern_13.java
@@ -0,0 +1,12 @@
+public class pattern_13 {
+    public static void main(String args[]){
+        int n=5;
+        char el=(char)('A'+n-1);
+        for(int i=1;i<=n;i++){
+            for(char ch=(char)(el-i+1);ch<=el;ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
