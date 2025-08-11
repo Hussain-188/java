edf5d1d96e3dca14cd9ded46f56f04a2602ff14f
Mohamed Hussain <23cs1242@stjosephstechnology.ac.in>
2025-08-11 12:55:52 +0530

    15th pattern

---
+++ src/pattern_15.java
@@ -0,0 +1,28 @@
+public class pattern_15 {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int s=1;s<=(2*n-2)-(2*i)+2;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }for(int i=1;i<n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+            for(int s=1;s<=i*2;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
